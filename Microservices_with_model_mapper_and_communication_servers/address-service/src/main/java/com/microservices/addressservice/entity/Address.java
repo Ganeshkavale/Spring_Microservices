66f893c8edcccadb76a4615bee5510d93766bf63
Ganesh <ganesh.kavale@sapours.co.in>
2024-02-28 16:08:16 +0530

    Microservices_with_model_mapper_and_communication_servers

---
+++ Microservices_with_model_mapper_and_communication_servers/address-service/src/main/java/com/microservices/addressservice/entity/Address.java
@@ -0,0 +1,79 @@
+package com.microservices.addressservice.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="address")
+public class Address {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+	@Column(name="id")
+	private int id;
+	 
+	@Column(name="post")
+	private String post;
+	
+	@Column(name="taluka")
+	private String taluka;
+	
+	@Column(name="district")
+	private String district;
+	
+	@Column(name="pin")
+	private String pin;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getPost() {
+		return post;
+	}
+
+	public void setPost(String post) {
+		this.post = post;
+	}
+
+	public String getTaluka() {
+		return taluka;
+	}
+
+	public void setTaluka(String taluka) {
+		this.taluka = taluka;
+	}
+
+	public String getDistrict() {
+		return district;
+	}
+
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+
+	public String getPin() {
+		return pin;
+	}
+
+	public void setPin(String pin) {
+		this.pin = pin;
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
