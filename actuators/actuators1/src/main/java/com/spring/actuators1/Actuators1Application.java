ad81cc101d56c058c60b6a6914b3a181284e6bdf
Ganesh <ganesh.kavale@sapours.co.in>
2024-02-28 16:11:55 +0530

    actuators

---
+++ actuators/actuators1/src/main/java/com/spring/actuators1/Actuators1Application.java
@@ -0,0 +1,13 @@
+package com.spring.actuators1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Actuators1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Actuators1Application.class, args);
+	}
+
+}
---
+++ actuators/actuators1/src/test/java/com/spring/actuators1/Actuators1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.actuators1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Actuators1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
